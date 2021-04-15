4141ecb1be6a0a90ce9fb6480f0c21d6c205fad6
Zsófi Zsigray <zsofi.zsigray@stud.uni-corvinus.hu>
2021-04-15 13:13:24 +0200

    8. gyak

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    // [Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            try
+            {
+                return new ContentResult
+                {
+                    ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                    Content = szoveg.ToUpper()
+                };
+            }
+            catch
+            {
+                return BadRequest("Nem jó a bemenő adat");
+            }
+        }
+
+    }
+}
--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,21 +29,27 @@ namespace HajosTeszt
 
 
             app.UseHttpsRedirection();
-            DefaultFilesOptions options = new DefaultFilesOptions();
-            options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("hajo.html");
-            app.UseDefaultFiles(options);
+           
+           
+            
+            app.UseDefaultFiles();
             app.UseStaticFiles(); //A sorrend fontos!
 
             app.UseRouting();
 
-           // app.UseEndpoints(endpoints =>
+            // app.UseEndpoints(endpoints =>
             //{
             //    endpoints.MapGet("/", async context =>
             //    {
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
         }
     }
 }
