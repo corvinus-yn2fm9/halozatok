a1fc16021adcd89eed16b4e13ee356f6660e11d8
Zsófi Zsigray <zsofi.zsigray@stud.uni-corvinus.hu>
2021-04-22 14:41:56 +0200

    9. hét

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,72 +1,83 @@
 ﻿var kérdések;
-var k =0;
+var kérdés;
+var kérdésszám;
 
-window.onload = () =>
-{
-    letöltés()
-    
+window.onload = () => {
+    letöltés();
+    kérdésBetöltés(1);
 }
 
+id = 1;
 
-
-function letöltés()
-{
-    fetch('/questions.json')
+function letöltés() {
+    fetch('/questions/all')
         .then(response => response.json())
-        .then(data => letöltésBefejeződött(data));
+        .then(data => { kérdésszám = data.length });
 }
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => feltöltés(data));
+}  
 
 function letöltésBefejeződött(d) {
     console.log("Sikeres letöltés")
     console.log(d)
     kérdések = d;
-    kérdésMegjelenítés(k);
+    kérdésBetöltés(1);
 }
 
-function kérdésMegjelenítés(k)
-{
-    document.getElementById("kérdés_szöveg").innerHTML = kérdések[k].questionText;
-    document.getElementById("válasz1").innerHTML = kérdések[k].answer1;
-    document.getElementById("válasz2").innerHTML = kérdések[k].answer2;
-    document.getElementById("válasz3").innerHTML = kérdések[k].answer3;
-    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[k].image;
+function feltöltés(kérdés) {
+    Kérdés = kérdés;
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
+
+
+következő = function () {
+    visszaszínezés()
+    id++;
+    if (id == kérdésszám) {
+        id = 0;
+    }
+
+    kérdésBetöltés(id);
+}
+
+előző = function () {
+    visszaszínezés()
+    id--;
+    if (id == 0) {
+        id = kérdésszám;
+    }
+
+    kérdésBetöltés(id);
+
+}
+
+ function színezés(valaszID) {
+     if (valaszID == kérdés.correctAnswer) { document.getElementById(valaszID).style.backgroundColor = "green" }
+     else { document.getElementById(valaszID).style.backgroundColor = "red" }
+}
+
+function visszaszínezés() {
     document.getElementById("válasz1").style.backgroundColor = "white";
     document.getElementById("válasz2").style.backgroundColor = "white";
     document.getElementById("válasz3").style.backgroundColor = "white";
 
 }
 
-
-következő = function() 
-{
-    k++;
-    if (k == kérdések.length)
-    {
-        k = 0;
-    }
-   
-    kérdésMegjelenítés(k);
-}
-
-előző = function ()
-{
-    k--;
-     if (k == 0)
-     {
-        k = kérdések.length;
-    }
-    
-    kérdésMegjelenítés(k);
-
-}
-
-színezés = function(n)
-{
-    if (n == kérdések[k].correctAnswer)
-    { document.getElementById("válasz"+n).style.backgroundColor = "green" }
-    else
-    { document.getElementById("válasz"+n).style.backgroundColor = "red" }
-}
 
 
 
